c225f4299f8a50aa0313c3fa5d072d3d1b6bdbc0
“Maribel” <maribel.sainzc@gmail.com>
2020-05-22 20:22:52 -0400

    style css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form id="form_main" class="content-header">
+      <div class="logo">
+        <img src="./assets/pokemon_logo.png" alt="Logo Pokemón" />
+      </div>
+
+      <div class="searchPokemon" for="form__principal">
+        <input type="text" id="form__principal" placeholder="Ingresa nombre de Pokemón" />
+
+        <button class="button-search">
+          <div class="button-content">
+            Buscar
+            <img src="./assets/pokebola.png" alt="Pokebola en botón" />
+          </div>
+        </button>
+      </div>
+    </form>
+
+    <section class="pokemonRespuesta"></section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App"
+  // components: {
+  //   HelloWorld
+  // }
+};
 </script>
 
-<style>
+<style src="./assets/style.css">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
