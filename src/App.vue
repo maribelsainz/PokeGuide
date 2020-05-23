57a1924754ed352ebdd3d134e90ba0b8e51e9d99
“Maribel” <maribel.sainzc@gmail.com>
2020-05-22 22:32:30 -0400

    se envia peticion y retorna respuesta de la API

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,60 @@
 <template>
   <div id="app">
-    <form id="form_main" class="content-header">
+    <div id="form_main" class="content-header">
       <div class="logo">
         <img src="./assets/pokemon_logo.png" alt="Logo Pokemón" />
       </div>
 
       <div class="searchPokemon" for="form__principal">
-        <input type="text" id="form__principal" placeholder="Ingresa nombre de Pokemón" />
+        <input
+          type="text"
+          id="form__principal"
+          placeholder="Ingresa nombre de Pokemón"
+          v-model="nombre"
+        />
 
-        <button class="button-search">
+        <button @click="pokebusca" class="button-search">
           <div class="button-content">
             Buscar
             <img src="./assets/pokebola.png" alt="Pokebola en botón" />
           </div>
         </button>
       </div>
-    </form>
+    </div>
 
-    <section class="pokemonRespuesta"></section>
+    <section class="pokemonRespuesta">
+      <h2>Movimientos</h2>
+      <h2>Habilidades</h2>
+    </section>
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "App"
-  // components: {
-  //   HelloWorld
-  // }
+  name: "App",
+  data() {
+    return {
+      nombre: "pikachu",
+    };
+  },
+  methods: {
+    pokebusca() {
+      if (this.nombre) {
+        axios
+          .get(`https://pokeapi.co/api/v2/pokemon/${this.nombre}`)
+          .then((response) => {
+            console.info(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+            alert("El nombre no existe");
+          });
+      } else {
+        alert("El pokémon no existe, ingresa otro nombre");
+      }
+    },
+  },
 };
 </script>
-
-<style src="./assets/style.css">
-#app {
-}
-</style>
+<style src="./assets/style.css"></style>
